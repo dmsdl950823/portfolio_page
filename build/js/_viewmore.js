b6f474405f2a5afb2c3b3981afcba3d4dd3c8e80
root <root@localhost.localdomain>
2020-03-09 07:42:16 +0900

    Upload build files

--- build/js/_viewmore.js
+++ build/js/_viewmore.js
@@ -1,18 +1,77 @@
- $(document).ready(function(){
-   $('.slider-for').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      fade: true,
-      asNavFor: '.slider-nav'
+
+
+// =========
+
+function jsonLoad() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState === 4 && this.status === 200) {
+            const data = JSON.parse(this.responseText);
+            const param = parseInt(window.location.hash.replace('#', ''));
+            console.log(param)
+            jsonapplier(data, param);
+        }
+    };
+    xhttp.open('GET', '../json/works.json', true);
+    xhttp.send();
+};
+
+function jsonapplier(data, param) {
+    let types = param;
+
+    // filtering data with type
+    const filtered_data = data.filter(_data => _data.work_type === types);
+
+
+    $(document).ready(function(){
+        // slider
+       $('.slider-for').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          fade: true,
+          asNavFor: '.slider-nav'
+        });
+
+        $('.slider-nav').slick({
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          asNavFor: '.slider-for',
+          // dots: true,
+          centerMode: true,
+          focusOnSelect: true
+        });
+
+        // mapping data into DOM
+        // let dataMap = [];
+        const mapped_data = filtered_data.map(_data => {
+
+            const largeimages = `<div>
+                                    <a href="/" class="page_anchor">
+                                        <img src="../images/${ _data.src }.jpg" alt=${ _data.id } width="500" />
+                                    </a>
+                                    <h3 class="largeTitle">${ _data.title }</h3>
+                                </div>`;
+
+            const smallimages = `<div>
+                                    <img src="../images/${ _data.src }.jpg" alt=${ _data.id } />
+                                    <p class="smallTitle"><span>${_data.title}</span></p>
+                                </div>`;
+
+
+
+            $('.slider-for').slick('slickAdd', largeimages);
+            $('.slider-nav').slick('slickAdd', smallimages);
+        });
+
+
     });
 
-    $('.slider-nav').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      asNavFor: '.slider-for',
-      dots: true,
-      centerMode: true,
-      focusOnSelect: true
-    });
-});
+
+
+    console.log(data)
+}
+
+
+jsonLoad();
+
