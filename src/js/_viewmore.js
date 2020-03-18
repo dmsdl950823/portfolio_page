aeafbad8584b69c1c249089101ecb214e202231e
root <root@localhost.localdomain>
2020-03-18 11:52:29 +0900

    Modified view language event, graph structure, data fetching

--- src/js/_viewmore.js
+++ src/js/_viewmore.js
@@ -1,15 +1,17 @@
 
-
-// =========
-
 function jsonLoad() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if(this.readyState === 4 && this.status === 200) {
-            const data = JSON.parse(this.responseText);
-            const param = parseInt(window.location.hash.replace('#', ''));
-            const title_param = decodeURI(window.location.hash).split('#')[2];
-            jsonapplier(data, param, title_param);
+            try {
+                const data = JSON.parse(this.responseText);
+                const param = parseInt(window.location.hash.replace('#', ''));
+                // console.log(data)
+                const title_param = decodeURI(window.location.hash).split('#')[2];
+                jsonapplier(data, param, title_param);
+            } catch (e) {
+                console.log(e)
+            }
         }
     };
     xhttp.open('GET', '../json/works.json', true);
@@ -19,6 +21,9 @@ function jsonLoad() {
 function jsonapplier(data, param, title_param) {
     let types = param;
 
+    console.log(data);
+
+    // Insert subTitle
     const subTitle = document.getElementById('subTitle');
     subTitle.innerHTML = title_param;
 
@@ -39,11 +44,11 @@ function jsonapplier(data, param, title_param) {
 
         $('.slider-nav').slick({
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           asNavFor: '.slider-for',
           dots: matchMedia,
           centerMode: true,
-          focusOnSelect: false
+          focusOnSelect: true
         });
 
         // mapping data into DOM
@@ -51,32 +56,59 @@ function jsonapplier(data, param, title_param) {
 
             let webData;
 
+            const browser_ = () => {
+                let browserList = [];
+                if (_data.browser !== null) {
+                    const browser = _data.browser;
+                    const browser_split = browser.split(', ');
+
+                    for (let i = 0; i <browser_split.length; i++ ){
+                        // console.log(browser_split[i])
+                        browserList.push(`<li>${browser_split[i]}</li>`);
+                    }
+                    return browserList.toString().replace(/,/g, '');
+                }
+            };
+
+
             if (title_param === 'Web Page') {
                 webData = `
                     <div class="dataContribution_wrap">
                         <div class="data_contribution">
                             <p>
                                 <strong>Participation</strong>
-                                <span> 30 %</span>
+                                <span>
+                                    ${_data.participate} %
+                                    <small class="g_back">
+                                        <i class="graph"></i>
+                                    </small>
+                                </span>
                             </p>
-                            <!--${_data.contribution}-->
                             <p>
                                 <strong>Coding</strong>
-                                <span>40 %</span>
+                                <span>
+                                    ${_data.coding} %
+                                    <small class="g_back">
+                                        <i class="graph"></i>
+                                    </small>
+                                </span>
                             </p>
                             <p>
                                 <strong>Design</strong>
-                                <span>50 %</span>
+                                <span>
+                                    ${_data.design} %
+                                    <small class="g_back">
+                                        <i class="graph"></i>
+                                    </small>
+                                </span>
                             </p>
                         </div>
                         <div class="desc_brwz_wrap">
-                            <p class="data_desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <p class="data_desc">${_data.description}</p>
                             <div class="browzer">
                                 <strong>CrossBrowser</strong>
                                 <ul>
-                                    <li>${_data.browser}</li>
-                                    <li>${_data.browser}</li>
-                                    <li>${_data.browser}</li>
+                                    ${browser_()}
                                 </ul>
                             </div>
                         </div>
@@ -87,39 +119,48 @@ function jsonapplier(data, param, title_param) {
                         <div class="data_contribution __data_contribution">
                             <p>
                                 <strong>Participation</strong>
-                                <span> 30 %</span>
+                                <span>
+                                    ${_data.participate}%
+                                    <small class="g_back">
+                                        <i class="graph"></i>
+                                    </small>
+                                </span>
                             </p>
-                            <!--${_data.contribution}-->
                             <p>
                                 <strong>Design</strong>
-                                <span>50 %</span>
+                                <span>
+                                    ${_data.design} %
+                                    <small class="g_back">
+                                        <i class="graph"></i>
+                                    </small>
+                                </span>
                             </p>
                         </div>
                         <div class="desc_brwz_wrap __desc_brwz_wrap">
-                            <p class="__data_desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <p class="__data_desc">${_data.description}</p>
                         </div>
                     </div>`;
             }
 
 
-            const largeimages = `<div>
+            const top_slide = `<div>
                                     <a href="/" class="page_anchor" target="_blank">
-                                        <img src="../images/${ _data.src }.jpg" alt=${ _data.id } />
+                                        <img src="../images/${ _data.img_src }.jpg" alt=${ _data.id } />
                                     </a>
                                     <h3 class="largeTitle">${ _data.title }</h3>
                                     <h4 class="dataSubTit">${ _data.subtitle }</h4>
                                     ${webData}
                                 </div>`;
 
-            const smallimages = `<div>
-                                    <img src="../images/${ _data.src }.jpg" alt=${ _data.id } />
+            const bottom_slide = `<div>
+                                    <img src="../images/${ _data.img_src }.jpg" alt=${ _data.id } />
                                     <p class="smallTitle"><span>${_data.title}</span></p>
                                 </div>`;
 
 
 
-            $('.slider-for').slick('slickAdd', largeimages);
-            $('.slider-nav').slick('slickAdd', smallimages);
+            $('.slider-for').slick('slickAdd', top_slide);
+            $('.slider-nav').slick('slickAdd', bottom_slide);
         });
 
 
